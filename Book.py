7d79dc119b1586731e384b4542d02d47594c08c1
Krzysztof <zamienny06@gmail.com>
2024-09-28 12:58:57 +0200

    Add two method to Book.py and modify init
    Modify init:
    - add id to Book
    - add status to Book
    - add limit to Reader
    - add borrowed_books to Reader
    
    Add a property for status
    Add a method add_points to Reader
    Add a method borow_the_books for Reader

--- Book.py
+++ Book.py
@@ -1,19 +1,53 @@
 import sqlite3
+from typing import List
+
 
 class Book:
 
     def __init__(
-            self, title: str, author: str, publisher: str, 
-            year_of_production: int):
+            self, id: int, title: str, author: str, publisher: str,
+            year_of_production: int, status: bool = True) -> None:
+        self.id = id
         self.title = title
         self.author = author
         self.publisher = publisher
         self.year_of_production = year_of_production
+        # True - (avaliable) can borrow | False (not avaliable) can't borrow
+        self.status = status
+
+    @property
+    def status(self):
+        """The status property."""
+        return self._status
+
+    @status.setter
+    def status(self, value):
+        self._status = value
 
 
 class Reader:
 
-    def __init__(self, name: str, surname: str):
+    def __init__(self, name: str, surname: str, limit: int = 2) -> None:
         self.name = name
         self.surname = surname
         self.points = 0
+        # Determines how many books can be borrowed at one time
+        self.limit = limit
+        self.borrowed_books: List[Book] = []
+
+    def add_point(self) -> None:
+        """method to add a point for the user"""
+        self.points += 1
+
+    def borow_the_books(self, book: Book):
+        """method for serving readers when borrowing books."""
+        if len(self.borrowed_books) < self.limit and book.status:
+            self.borrowed_books.append(book)
+            self.add_point()
+            book.status = False
+        else:
+            if len(self.borrowed_books) == self.limit:
+                print("You can't boorow the book because"
+                      f"you have {self.limit} books")
+            else:
+                print("This book isn't available")
